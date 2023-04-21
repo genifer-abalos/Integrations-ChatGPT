50f8454726d6616cb2bf844ae535380f67b9834d
Genifer Abalos <genifer.abalos@gmail.com>
2023-04-22 05:11:01 +0800

    draft of functionalities

---
+++ slack/slack-bot.py
@@ -0,0 +1,107 @@
+import json
+import openai
+import slack
+from slack import WebhookClient
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+import os
+from dotenv import load_dotenv
+import ssl
+import certifi
+from datetime import date
+import time
+
+from flask import Flask, request, make_response
+app = Flask(__name__)
+
+load_dotenv()
+
+from slack_sdk.signature import SignatureVerifier
+signature_verifier = SignatureVerifier(
+    signing_secret=os.environ['SLACK_SIGNING_SECRET']
+)
+
+
+# add this line to point to your certificate path
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+ssl_context2 = ssl.create_default_context()
+ssl_context2.check_hostname = False
+ssl_context2.verify_mode = ssl.CERT_NONE
+
+
+client = slack.WebClient(os.environ['SLACK_BOT_TOKEN'], ssl=ssl_context2)
+# slack_app = App(token=os.environ['SLACK_API_TOKEN'])
+openai.api_key = os.environ['CHATGPT_API_KEY']
+
+channel_url = "https://hooks.slack.com/services/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"
+
+'''webhook = WebhookClient(channel_url, ssl=ssl_context)
+response = webhook.send(
+    text="fallback",
+    blocks=[
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "You have a new request:\n*<fakeLink.toEmployeeProfile.com|Fred Enriquez - New device request>*"
+            }
+        }
+    ]
+)'''
+
+t = time.localtime()
+current_time = time.strftime("%H:%M:%S", t)
+
+# client.chat_postMessage(channel="#chatgpt-convo", text="Hi!")
+client.chat_postMessage(channel="#chatgpt-convo", text="The current date is " + str(date.today()) + " " + str(current_time))
+
+
+def get_chatgpt_response(the_prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=the_prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5).choices[0].text
+
+    # Reply to thread
+    response = client.chat_postMessage(channel="#chatgpt-convo", text=f"Here you go: \n{response}")
+    print(response.data)
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    print(f"Connecting webhooks")
+    request_body = request.data
+    print(f"{request_body = }")
+
+    return request_body
+
+
+@app.route("/slack/events", methods=["POST"])
+def slack_app():
+    # Verify incoming requests from Slack @mentions
+    print(f"Connecting /slack/events")
+    request_body = request.data
+    print(f"{request_body = }")
+
+    prompt = json.loads(request_body)['event']['text']
+    get_chatgpt_response(the_prompt=prompt)
+
+    return request_body
+
+
+if __name__ == '__main__':
+    # Get event from Slack
+    # Get the prompt from Slack mentions
+    # Pass it to ChatGPT API
+    # Fetch ChatGPT response
+    # Post response to slack channel and thread
+
+    # SocketModeHandler(slack_app, os.environ['SLACK_API_TOKEN']).start()
+
+    app.run(debug=True, port=8081)
+
+    prompt = "What are some cool python projects?"
+    # get_chatgpt_response(the_prompt=prompt)
